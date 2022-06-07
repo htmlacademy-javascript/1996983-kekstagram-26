24e57a13bcabdb6f737f452bf7ff0abf18162f1b
Марина Ашихина <markopcik@mail.ru>
2022-06-07 20:53:56 +0500

    Исправление функции

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 //Функция, возвращающая случайное целое число из переданного диапазона
 
-function getRandomInt(min, max) {
-  if (max < 0 || min < 0) {
-    return;
+const getRandomInt = (min, max)  => {
+  if (min < 0 || max < 0) {
+    return false;
   }
 
   return (max > min) ? Math.floor(Math.random() * (max - min + 1) + min)
     : Math.floor(Math.random() * (min - max + 1) + max);
-}
+};
 
 getRandomInt(10, 20);
+
 
 // Функция проверки длины введённого комментария
 
