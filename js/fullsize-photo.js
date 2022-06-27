207427fa8e333d11bcf72f4d730b7a5c6311b4a4
Марина Ашихина <markopcik@mail.ru>
2022-06-27 18:48:18 +0500

    Отрисуй меня полностью

---
+++ js/fullsize-photo.js
@@ -0,0 +1,48 @@
+const body = document.querySelector('body');
+const photoContainer = document.querySelector('.big-picture');
+const photoCloseButton = photoContainer.querySelector('#picture-cancel');
+const photoImg = photoContainer.querySelector('.big-picture__img img');
+const photoDescription = photoContainer.querySelector('.social__caption');
+const photoLikesCount = photoContainer.querySelector('.likes-count');
+const commentCount = photoContainer.querySelector('.social__comment-count');
+const commentContainer = photoContainer.querySelector('.social__comments');
+const commentItem = photoContainer.querySelector('.social__comment');
+const commentLoader = photoContainer.querySelector('.comments-loader');
+const commentFragment = document.createDocumentFragment();
+
+const closePhoto = () => {
+  photoContainer.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', photoCloseHandler);
+};
+
+function photoCloseHandler(evt) {
+  if (evt.key === 'Escape') {
+    closePhoto();
+  }
+}
+
+photoCloseButton.addEventListener('click', closePhoto);
+const renderFullSizePhoto = ({ url, description, likes, comments }) => {
+  photoContainer.classList.remove('hidden');
+  commentCount.classList.add('hidden');
+  commentLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+  photoImg.src = url;
+  photoDescription.textContent = description;
+  photoLikesCount.textContent = likes;
+
+  comments.forEach(({ message, avatar, name }) => {
+    const commentClone = commentItem.cloneNode(true);
+    const commentCloneAvatar = commentClone.querySelector('img');
+    commentCloneAvatar.src = avatar;
+    commentCloneAvatar.alt = name;
+    commentClone.querySelector('.social__text').textContent = message;
+    commentFragment.append(commentClone);
+  });
+  commentContainer.innerHTML = '';
+  commentContainer.append(commentFragment);
+  document.addEventListener('keydown', photoCloseHandler);
+};
+
+export { renderFullSizePhoto };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { photoDescription } from './data.js';
+import { renderThumbanails } from './thumbanails.js';
 
-import './picture-rendering.js';
+renderThumbanails(photoDescription);
 
 // eslint-disable-next-line no-console
 console.log(photoDescription);
