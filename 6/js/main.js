11b7c59ca196b71e554102446c8b2a3244296747
Keks <keks@htmlacademy.ru>
2022-06-28 08:57:25 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import { photoDescription } from './data.js';
+import { renderThumbanails } from './thumbanails.js';
+
+renderThumbanails(photoDescription);
+
+// eslint-disable-next-line no-console
+console.log(photoDescription);
