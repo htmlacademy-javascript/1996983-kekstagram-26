22f2a5586267380c48dd1237fb4e6d8c4a45f1ef
Марина Ашихина <markopcik@mail.ru>
2022-07-11 18:45:14 +0500

    Правки

--- js/form.js
+++ js/form.js
@@ -3,6 +3,8 @@ import { FormValidation } from './validation.js';
 import { addScaleHandler, removeScaleHandler } from './changing-image-scale.js';
 import { setNoneEffect } from './image-slider-effects.js';
 
+const PREVIEW_SCALE_DEFAULT = 100;
+
 const body = document.querySelector('body');
 const uploadPhotoFormNode = document.querySelector('#upload-select-image');
 const uploadPhotoFileNode = document.querySelector('#upload-file');
@@ -10,6 +12,17 @@ const photoEditContainerNode = document.querySelector('.img-upload__overlay');
 const cancelPhotoButton = photoEditContainerNode.querySelector('#upload-cancel');
 const textHashtags = uploadPhotoFormNode.querySelector('.text__hashtags');
 const textComment = uploadPhotoFormNode.querySelector('.text__description');
+const scaleControlValue = photoEditContainerNode.querySelector('.scale__control--value');
+const photoPreviewImageNode = photoEditContainerNode.querySelector('.img-upload__preview');
+
+const clearEnterData = () => {
+  scaleControlValue.value = `${PREVIEW_SCALE_DEFAULT}%`;
+  photoPreviewImageNode.style = 'transform: scale(1)';
+
+  uploadPhotoFormNode.reset();
+  photoPreviewImageNode.style.filter = 'none';
+  photoPreviewImageNode.src = '';
+};
 
 const onPhotoEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -24,6 +37,7 @@ function cancelPhotoContainer() {
   document.removeEventListener('keydown', onPhotoEscKeydown);
   uploadPhotoFormNode.reset();
   removeScaleHandler();
+  clearEnterData();
 }
 
 cancelPhotoButton.addEventListener('click', () => cancelPhotoContainer());
